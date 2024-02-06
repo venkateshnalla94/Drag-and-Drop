b4b87aeb4db9cc49cc76e40061c1e1c67c285fef
Venkatesh Nalla <venalla@deloitte.ca>
2024-02-06 00:08:23 -0500

    Drag and handle drop

--- src/app.ts
+++ src/app.ts
@@ -61,6 +61,17 @@ class ProjectState extends State<Project> {
     );
 
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id == projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+    }
+    this.updateListeners();
+  }
+  updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -223,8 +234,13 @@ class ProjectList
     listEl.classList.remove("droppable");
   }
 
+  @AutoBind
   dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData("text/plain"));
+    const prjId = event.dataTransfer!.getData("text/plain");
+    projectState.moveProject(
+      prjId,
+      this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+    );
   }
 
   configure(): void {
